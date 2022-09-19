477387183a29ddd426b62fe5dff737036b1aa39d
mrx7355608 <mrx7355608@gmail.com>
2022-09-19 17:07:35 +0500

    made changes to some files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# nodejs-scraper
+# Nodejs Scraper
+Built a nodejs web scraper with:
+- Cheerio
+- Request-promise
+
+> **Note:**
+Amazon scraper is just a hack which extracts data from amazon with the help of DOM manipulation in JavaScript
+
+## Getting started
+
+First install all dependencies from npm
+```
+npm install
+```
+Add the urls you want to scrape in `czoneScraper.js` file
+> **Important!** Have a look at the links provided in the script to know what type of links are required! Incorrect links will cause issues.
+
+After all the things are setup
+
+Run the `czoneScraper.js` file with a simple nodejs command
+```
+node czoneScraper.js
+```
+
--- czoneScraper.js
+++ czoneScraper.js
@@ -1,53 +1,54 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
-const fsPromises = require("fs/promises")
+const rp = require("request-promise");
+const cheerio = require("cheerio");
+const fsPromises = require("fs/promises");
 
 const urls = [
-  "https://www.czone.com.pk/graphic-cards-pakistan-ppt.154.aspx",
-  "https://www.czone.com.pk/graphic-cards-pakistan-ppt.154.aspx?page=2",
-  "https://www.czone.com.pk/graphic-cards-pakistan-ppt.154.aspx?page=3"
-]
+    "https://www.czone.com.pk/graphic-cards-pakistan-ppt.154.aspx",
+    "https://www.czone.com.pk/graphic-cards-pakistan-ppt.154.aspx?page=2",
+    "https://www.czone.com.pk/graphic-cards-pakistan-ppt.154.aspx?page=3",
+];
 
+const parseHtml = (html) => {
+    const $ = cheerio.load(html);
 
-const parseHtml = html => {
-  const $ = cheerio.load(html);
+    const image = $(".image > a > img").attr("src");
+    const title = $("h4").text();
+    const desc = $("div.description").text().trim();
+    const stock = $("div.product-stock > span.product-data").text();
+    const price = $("div.price").text().trim();
+    const ratings = $("div.rating > span.product-rating").text() || 0;
+    const totalReviews = $("span.first").text() || 0;
+    const link = $("h4 > a").attr("href");
+    const featuresList = $("ul.description.highlights.text-left")
+        .children()
+        .toArray();
+    const features = featuresList.map((elem) => $(elem).text());
 
-  const image = $(".image > a > img").attr("src");
-  const title = $("h4").text();
-  const desc = $("div.description").text().trim();
-  const stock = $("div.product-stock > span.product-data").text();
-  const price = $("div.price").text().trim();
-  const ratings = $("div.rating > span.product-rating").text() || 0;
-  const totalReviews = $("span.first").text() || 0;
-  const link = $("h4 > a").attr("href")
-  const featuresList = $("ul.description.highlights.text-left").children().toArray();
-  const features = featuresList.map((elem) => $(elem).text())
-
-  return {
-    image,
-    title,
-    desc,
-    price,
-    stock,
-    ratings,
-    totalReviews,
-    tag: "czone",
-    link,
-    features,
-  }
-}
+    return {
+        image,
+        title,
+        desc,
+        price,
+        stock,
+        ratings,
+        totalReviews,
+        tag: "czone",
+        link,
+        features,
+    };
+};
 
 const scrapeData = async (url) => {
-  const html = await rp(url)
-  const $ = cheerio.load(html)
-  const itemsList = $('div.product').toArray();
-  const data = itemsList.map(item => parseHtml(item));
-  await writeJsonFile(JSON.stringify(data, null, 4))
-}
+    const html = await rp(url);
+    const $ = cheerio.load(html);
+    const itemsList = $("div.product").toArray();
+    const data = itemsList.map((item) => parseHtml(item));
+    await writeJsonFile(JSON.stringify(data, null, 4));
+};
 
 async function writeJsonFile(content) {
-  await fsPromises.appendFile("./czone-graphic-cards.json", content);
-  console.log("Data has been saved!")
+    await fsPromises.appendFile("./czone-data.json", content);
+    console.log("Data has been saved!");
 }
 
-urls.forEach(url => scrapeData(url))
+urls.forEach((url) => scrapeData(url));
